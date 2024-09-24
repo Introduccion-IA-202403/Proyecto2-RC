10ed82459a797db6267c48d24fe8ea6b00400090
TalkySafe143 <sebastian.galindo143@gmail.com>
2024-09-23 22:26:58 -0500

    Normalize logic 2/2

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <vector>
 #include <Clause.h>
+#include <LogicTransform.h>
 
 using namespace std;
 
 int main() {
+    LogicTransform transform;
     vector<Clause*> base;
     cout << "Ingrese el nombre del archivo con la base: ";
     string filename; cin >> filename;
@@ -20,6 +22,14 @@ int main() {
         base.push_back(new Clause(clause, i));
         cout << "Clausula " << clause << " añadida" << endl;
     }
+
+    vector< vector< Clause > > normalBase;
+
+    for (auto cl: base) {
+        auto trns = transform.transformClause(cl);
+        for (auto cls: trns) normalBase.push_back(cls);
+    }
+
 
     // Limpiar la memoria
     for (auto & cl : base) delete cl;
